80e4c7d074ed8efce460f4db4942db32ae9d4c38
olimov Amir <olimov.amir@mail.ru>
2023-12-01 10:42:35 +0300

    Добавьте файлы проекта.

---
+++ Null.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Null__Coalescing
+{
+     class Null
+    {
+        static int[] GetArray() 
+        {
+            int[] myArray = new int[0];
+
+            return myArray;
+        }
+        static void Main(string[] args)
+        {
+            string str = null;
+
+             str ??= string.Empty;
+            Console.WriteLine("Count simbol in string " + str.Length);
+
+            int[] myArray = GetArray();
+
+            myArray ??= new int[0];
+
+            Console.WriteLine("Count simbol in array " + myArray.Length);
+        }
+    }
+}
